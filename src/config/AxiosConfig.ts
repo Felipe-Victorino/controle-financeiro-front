84a46b98abe83b73542c4d3c428736c5b2256591
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-07-20 14:27:20 -0300

    feat(AxiosConfig.ts): Axios config for HTTPS requests

---
+++ src/config/AxiosConfig.ts
@@ -0,0 +1,26 @@
+import axios, { type AxiosInstance } from 'axios';
+
+
+const api : AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_REACT_APP_API_BASE_URL || 'http://localhost:8080',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('app-token');
+    const usuario = JSON.parse(localStorage.getItem('usuario') || 'null');
+    const tokenUsuario = usuario?.token;
+
+    if (token || tokenUsuario) {
+      config.headers.Authorization = `Bearer ${token || tokenUsuario}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
