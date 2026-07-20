59fc42f9cef90f86f4986f00dcf8655999e54785
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-07-20 14:24:49 -0300

    feat(AsyncButton.tsx): Component for Button that needs to do asynchronous tasks

---
+++ src/components/Button/AsyncButton.tsx
@@ -0,0 +1,40 @@
+import "./Button.css";
+import React from "react";
+import LoadingThrobber from "../LoadingThrobber/LoadingThrobber.tsx";
+
+interface AsyncButtonTypes extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    type: "submit" | "reset" | "button" | undefined,
+    onClick: (e: React.MouseEvent<Element, MouseEvent>) => Promise<void>
+    isLoading?: boolean;
+
+}
+
+const AsyncButton: React.FC<AsyncButtonTypes> = (
+    {
+        children,
+        disabled,
+        type,
+        onClick = async () => undefined,
+        isLoading = false,
+        ...props
+    }: AsyncButtonTypes) => {
+    return (
+        <button
+            disabled={disabled}
+            className={"button-main"}
+            type={type}
+            onClick={
+                (e: React.MouseEvent<Element, MouseEvent>) => {
+                    onClick(e)
+                }
+            }
+            {...props}
+        >
+
+            {isLoading ? (<LoadingThrobber/>) : (children)}
+
+        </button>
+    )
+}
+
+export default AsyncButton;
