44b87819648d8ad4662dd2d6b486a097fe09625a
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-04 12:25:53 -0300

    fix( BackButton.tsx ): old number method didnt really work very well, changed to a string route name instead

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -1,18 +1,18 @@
 import BackArrow from "../Icons/Back.tsx";
-import {useNavigate} from "react-router-dom";
+import {type NavigateOptions, useNavigate} from "react-router-dom";
 
-interface BackButtonTypes {
-    n?: number
+interface BackButtonTypes extends NavigateOptions {
+    to: string
 }
 
-const BackButton = ({n}: BackButtonTypes) => {
+const BackButton = ({to}: BackButtonTypes) => {
     const navigate = useNavigate()
     return (
         <button
             className={"flex items-center justify-center rounded-full p-3 hover:bg-slate-300 dark:hover:bg-slate-600"}
             type={"button"}
             onClick={() => {
-                navigate(n || -1)
+                navigate(to)
             }}><BackArrow/></button>
     )
 }
