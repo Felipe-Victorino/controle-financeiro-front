761702904d6e57c4569c11182787017dd041bffc
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-04 13:08:52 -0300

    feat( App.tsx ): changed styling for the netire app structure, to fit the header and navbar changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,30 @@ import Header from "./components/Header/Header";
 import NewUser from "./pages/NewUser/NewUser";
 import RequestPassword from "./pages/RequestPassword/RequestPassword.tsx";
 import Dashboard from "./pages/Dashboard/Dashboard.tsx";
+import ChangePassword from "./pages/ChangePassword/ChangePassword.tsx";
 
 function App() {
     return (
-        <div className="bg-emerald-100 dark:bg-mist-900 min-h-screen flex flex-col">
-            <Header title={"FinFin"}/>
-            <main className="grow p-8">
+        <div className="app">
+
+            <main className="h-full">
+
                 <BrowserRouter>
+                    <Header title={"FinFin"}/>
                     <Routes>
-                        <Route path="/" element={<Login/>}></Route>
-                        <Route path="/signin" element={<NewUser/>}></Route>
-                        <Route path="/forgot" element={<RequestPassword/>}></Route>
-                        <Route path="/dash" element={<Dashboard/>}></Route>
+
+                        <Route index path={"/"} element={<Login/>}></Route>
+                        <Route path={"auth"}>
+                            <Route path="login" element={<Login/>}></Route>
+                            <Route path="signin" element={<NewUser/>}></Route>
+                            <Route path="forgot" element={<RequestPassword/>}></Route>
+                            <Route path="reset" element={<ChangePassword/>}></Route>
+                        </Route>
+
+
+                        <Route path="dash" element={<Dashboard/>}>
+
+                        </Route>
                     </Routes>
                 </BrowserRouter>
             </main>
