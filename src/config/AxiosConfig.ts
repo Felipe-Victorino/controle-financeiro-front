1b7b3f1801dd9a4949b6389968b53f7a28abedbc
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-04 12:25:17 -0300

    fix( AxiosConfig.ts ): formatting

--- src/config/AxiosConfig.ts
+++ src/config/AxiosConfig.ts
@@ -1,26 +1,26 @@
-import axios, { type AxiosInstance } from 'axios';
+import axios, {type AxiosInstance} from 'axios';
 
 
-const api : AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_API_BASE_URL || 'http://localhost:8080',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+const api: AxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_REACT_APP_API_BASE_URL || 'http://localhost:8080/api/v1',
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('app-token');
-    const usuario = JSON.parse(localStorage.getItem('usuario') || 'null');
-    const tokenUsuario = usuario?.token;
+    (config) => {
+        const token = localStorage.getItem('app-token');
+        const usuario = JSON.parse(localStorage.getItem('usuario') || 'null');
+        const tokenUsuario = usuario?.token;
 
-    if (token || tokenUsuario) {
-      config.headers.Authorization = `Bearer ${token || tokenUsuario}`;
-    }
+        if (token || tokenUsuario) {
+            config.headers.Authorization = `Bearer ${token || tokenUsuario}`;
+        }
 
-    return config;
-  },
-  (error) => Promise.reject(error)
+        return config;
+    },
+    (error) => Promise.reject(error)
 );
 
 export default api;
