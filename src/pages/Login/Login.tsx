cb5128c018606d89137646fceabb26990976b311
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-04 13:11:37 -0300

    feat( Login.tsx ): Added error messages, the new input component and changed page styling

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,59 +1,58 @@
 import TextInput from "../../components/Input/TextInput";
 import Card from "../../components/Card/Card";
-import "../../components/TextHeadings/TextHeadings.css";
+import "../../styles/TextHeadings.css";
 import "./Login.css";
-
+import "../../styles/Pages.css"
 
 import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 import AsyncButton from "../../components/Button/AsyncButton.tsx";
+import ErrorMessage from "../../components/Warning/ErrorMessage.tsx";
+import PasswordInput from "../../components/Input/PasswordInput.tsx";
 
 
 const Login = () => {
-    const navigator = useNavigate();
     return (
-        <div>
-            <div className="h-1/1 flex flex-row justify-center items-middle">
-                <div className="flex flex-col justify-center items-middle">
 
+        <div className="page-fixed-center-start">
+            <div className="flex flex-col justify-center items-middle">
+                <Card className={"grid grid-cols-1 auto-rows-min md:grid-cols-2 gap-6"}>
+                    <div
+                        className={"row-start-2 md:col-start-1 md:row-start-2 flex flex-col justify-center items-stretch gap-6"}>
+                        <div>
+                            <h1 className="header-title">FinFin</h1>
+                            <h2 className="header-subtitle ">Logar no serviço</h2>
+                        </div>
+                    </div>
 
-                    <Card className={"grid grid-cols-1 auto-rows-min md:grid-cols-2 gap-6"}>
-                        <div
-                            className={"row-start-2 md:col-start-1 md:row-start-2 flex flex-col justify-center items-stretch gap-6"}>
-                            <div>
-                                <h1 className="header-title">FinFin</h1>
-                                <h2 className="header-subtitle ">Logar no serviço</h2>
-                            </div>
-
+                    <div className={"row-start-3 md:col-start-2 md:row-start-2 flex flex-col gap-6"}>
+                        <LoginForm/>
+                        <div className={"flex flex-col gap-1"}>
+                            <Link to={"/auth/signin"} className="link">
+                                Registrar novo usuário
+                            </Link>
+                            <Link to={"/auth/forgot"} className="link">
+                                Esqueci minha senha
+                            </Link>
 
                         </div>
+                    </div>
 
-                        <div className={"row-start-3 md:col-start-2 md:row-start-2 flex flex-col gap-6"}>
-                            <LoginForm/>
-                            <div className={"flex flex-col gap-1"}>
-                                <a href="" className="forgot-passwd" onClick={() => {
-                                    navigator("/signin")
-                                }}>Registrar novo usuário</a>
-                                <a href="" className="forgot-passwd" onClick={() => {
-                                    navigator("/forgot")
-                                }}>Esqueci minha senha</a>
-                            </div>
-                        </div>
+                </Card>
 
-                    </Card>
-
-                </div>
             </div>
         </div>
+
     );
 };
 
 const LoginForm = () => {
     const [user, setUser] = useState({login: "", passwd: ""});
-
+    const [error, setError] = useState("");
 
     const verifyLogin = async () => {
         console.log("Login");
+        setError("Não Implementado")
     }
     /*
    makeLogin() => {
@@ -67,6 +66,7 @@ const LoginForm = () => {
 
     return (
         <form className="flex flex-col">
+
             <TextInput
                 label={"Login"}
                 id="login"
@@ -76,18 +76,17 @@ const LoginForm = () => {
                 onChangeInput={(e) => handleChange(e)}
             />
 
-            <TextInput
+            <PasswordInput
                 label={"Senha"}
                 id="senha"
                 name="passwd"
                 value={user.passwd}
-                inputtype="password"
-                onChangeInput={(e) => handleChange(e)}
+                onChanged={(e) => handleChange(e)}
             />
             <AsyncButton type="submit" onClick={() => verifyLogin()}>
                 Entrar
             </AsyncButton>
-
+            <ErrorMessage>{error}</ErrorMessage>
 
         </form>
     );
