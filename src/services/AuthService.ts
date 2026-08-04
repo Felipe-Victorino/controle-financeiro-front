0b3d6fe7fc3638714c703d80af5e3f419ad30e0a
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-04 13:09:52 -0300

    feat( AuthService.ts ): specific functions for handling the different endpoints for authentication

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,10 +1,28 @@
 import BaseService from "./BaseService";
+import type {AxiosResponse} from "axios";
 
-class AuthService extends BaseService{
-  constructor(){
-    super('/auth');
+class AuthService extends BaseService {
 
-  }
+    constructor() {
+        super('/auth');
+
+    }
+
+    async requestResetCode(email: string): Promise<AxiosResponse> {
+        return await this.insertIn("/forgot-password", {"email": email});
+    }
+
+    async registerNewUser(data: object): Promise<AxiosResponse> {
+        return await this.insertIn("/register", data);
+    }
+
+    async resetPassword(data: object): Promise<AxiosResponse> {
+        return await this.insertIn("/reset-password", data);
+    }
+
+    async loginUser(data: object): Promise<AxiosResponse> {
+        return await this.insertIn("/login", data);
+    }
 }
 
 export default AuthService;
