d9a868613d21c6d4aaf78148f9f984582ef1cdd9
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-08-11 16:09:14 -0300

    feat( components/tailwind/* ): Removing old tailwind components

--- src/components/tailwind/Icons/Back.tsx
+++
@@ -1,9 +0,0 @@
-import 'material-symbols/outlined.css';
-import "./Icons.css"
-
-const BackArrow = () => {
-    return (
-        <span className="material-symbols-outlined icons"> arrow_back </span>
-    )
-}
-export default BackArrow
