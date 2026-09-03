56654a410b8398d0da710f58c01e59ce8f541a34
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-09-03 14:27:52 -0300

    feat( App.tsx ): Setup a redirect if the app detects youre logged in already via local storage

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,37 @@ import "./App.css";
 
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-import Dashboard from "./pages/Dashboard/Dashboard.tsx";
+import PageDashboard from "./pages/Dashboard/PageDashboard.tsx";
 
 import PageLogin from "@/pages/Auth/Login/PageLogin.tsx";
 import {Box} from "@chakra-ui/react";
 import PageRegister from "@/pages/Auth/NewUser/PageRegister.tsx";
 import PageForgotPassword from "@/pages/Auth/ForgotPassword/PageForgotPassword.tsx";
 import PageResetPassword from "@/pages/Auth/ChangePassword/PageResetPassword.tsx";
+import {useEffect, useState} from "react";
+import PageCategories from "@/pages/Category/PageCategories.tsx";
 
 
 function App() {
+
+    const [indexElement, setIndexElement] = useState(<PageLogin/>)
+
+    useEffect(() => {
+        function changeRootElement() {
+            if (localStorage.getItem("login") === "true") {
+                setIndexElement(<PageDashboard/>)
+            }
+        }
+
+        changeRootElement()
+    }, []);
+
     return (
         <Box h={"100vh"}>
             <BrowserRouter>
                 <Routes>
 
-                    <Route index path={"/"} element={<PageLogin/>}></Route>
+                    <Route index path={"/"} element={indexElement}></Route>
                     <Route path={"auth"}>
                         <Route path="login" element={<PageLogin/>}></Route>
                         <Route path="signin" element={<PageRegister/>}></Route>
@@ -26,9 +41,12 @@ function App() {
                     </Route>
 
 
-                    <Route path="dash" element={<Dashboard/>}>
+                    <Route path="dashboard" element={<PageDashboard/>}>
 
                     </Route>
+                    <Route path={"categories"} element={<PageCategories/>}/>
+
+
                 </Routes>
             </BrowserRouter>
         </Box>
