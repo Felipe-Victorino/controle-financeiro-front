0c97eed1663534780afc60b3eabb7e800ba93633
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-09-03 14:34:49 -0300

    feat( FormContainer.tsx ): Forms fill the whole page, lining up with the header

--- src/components/Containers/FormContainer.tsx
+++ src/components/Containers/FormContainer.tsx
@@ -1,26 +1,21 @@
 import type {ReactNode} from "react";
-import {Box, Center, Flex} from "@chakra-ui/react";
+import {Center, Flex} from "@chakra-ui/react";
 
 const FormContainer = ({children}: { children: ReactNode }) => {
     return (
-        <Center w={"100%"} h={"100%"}>
-            <Box
-                h={{base: "95%", sm: "70%", md: "70%"}}
-                w={{base: "95%", sm: "85%", md: "70%", lg: "60%"}}
-                padding={"12"}
+        <Flex h={"full"}>
+            <Center
+                h={{base: "100%", sm: "70%", md: "70%"}}
+                w={{base: "100%"}}
                 rounded={"md"}
                 shadow={"lg"}
                 backgroundColor={"bg.panel"}
             >
-                <Center w={"100%"} h={"100%"}>
-                    <Flex direction={{base: "column", lg: "row"}} gap={{base: "24"}}>
-                        {children}
-                    </Flex>
-
-                </Center>
-
-            </Box>
-        </Center>
+                <Flex direction={{base: "column", lg: "row"}} gap={{base: "24"}}>
+                    {children}
+                </Flex>
+            </Center>
+        </Flex>
     )
 }
 
