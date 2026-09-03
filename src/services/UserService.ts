e3a86c886eb614e0e8248f74430591ff60bd10d5
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-09-03 14:22:54 -0300

    feat( UserService.ts ): userService is now a singleton

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,8 +1,19 @@
 import BaseService from "./BaseService";
 
 class UserService extends BaseService{
-  constructor(){
+
+  static #instance: UserService;
+
+  private constructor(){
     super('/user');
+  }
+
+  public static get instance(){
+    if(!UserService.#instance){
+      this.#instance = new UserService();
+    }
+
+    return this.#instance;
   }
 }
 
