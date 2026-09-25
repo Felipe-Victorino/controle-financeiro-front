e82521caaaf45500bae85fd1b320e731cf1196b3
Felipe-Victorino <felipe.flvictorino@gmail.com>
2026-09-24 22:22:08 -0300

    feat ( PageDashboard.tsx ): Added a carousel, that points to locations of interest

--- src/pages/Dashboard/PageDashboard.tsx
+++ src/pages/Dashboard/PageDashboard.tsx
@@ -6,6 +6,9 @@ import DashboardContainer from "@/components/Containers/DashboardContainer.tsx";
 import DashboardTitle from "@/components/DashboardTitle.tsx";
 import RecentTransactions from "@/components/DataCards/RecentTransactions.tsx";
 import FinanceSummary from "@/components/DataCards/FinanceSummary.tsx";
+import {Button, Card, Carousel, Heading, IconButton} from "@chakra-ui/react";
+import type {LocationLinkCardType} from "@/types/DataCards.ts";
+import {MdArrowBack, MdArrowForward} from "react-icons/md";
 
 const PageDashboard = () => {
     const navigate = useNavigate();
@@ -31,12 +34,97 @@ const PageDashboard = () => {
             }
             >
 
+                <ActionsRow/>
                 <FinanceSummary/>
 
                 <RecentTransactions/>
             </DashboardContainer>
 
         </PageContainer>
+    )
+}
+
+
+const LocationCard = ({data}: { data: LocationLinkCardType }) => {
+    const navigate = useNavigate();
+    return (
+        <Card.Root>
+            <Card.Body>
+                <Card.Title>
+                    <Heading>
+                        {data.name}
+                    </Heading>
+
+                </Card.Title>
+                <Card.Description>
+                    {data.description}
+                </Card.Description>
+            </Card.Body>
+            <Card.Footer justifyContent={"flex-end"}>
+                <Button onClick={() => {
+                    navigate(data.link);
+                }
+                }>
+                    Ver mais
+                </Button>
+
+            </Card.Footer>
+
+        </Card.Root>
+    )
+}
+
+const locations: LocationLinkCardType[] = [
+    {
+        name: "Perfil",
+        description: "Gerencie as suas preferências e dados",
+        link: "/user"
+    },
+    {
+        name: "Categorias",
+        description: "Gerencie as suas categorias pessoais",
+        link: "/categories"
+    },
+    {
+        name: "Carteiras",
+        description: "Gerencie as suas carteiras e carteiras que você pertence",
+        link: "/wallets"
+    },
+    {
+        name: "Transações",
+        description: "Gerencie as suas transações realizadas",
+        link: "/transactions"
+    }
+]
+
+const ActionsRow = () => {
+
+    return (
+        <Carousel.Root slideCount={locations.length} slidesPerPage={3} gap={"0.5rem"}>
+            <Carousel.Control justifyContent="center" gap="4" width="full">
+                <Carousel.PrevTrigger asChild>
+                    <IconButton size="xs" variant="outline">
+                        <MdArrowBack/>
+                    </IconButton>
+                </Carousel.PrevTrigger>
+
+                <Carousel.ItemGroup width="full">
+                    {locations.map((_src, index) => (
+                        <Carousel.Item key={index} index={index}>
+                            <LocationCard data={_src}/>
+                        </Carousel.Item>
+                    ))}
+                </Carousel.ItemGroup>
+
+                <Carousel.NextTrigger asChild>
+                    <IconButton size="xs" variant="outline">
+                        <MdArrowForward/>
+                    </IconButton>
+                </Carousel.NextTrigger>
+            </Carousel.Control>
+
+            <Carousel.Indicators/>
+        </Carousel.Root>
     )
 }
 
